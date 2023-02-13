0130331d7aba89c63a2fa3b5043b12460d7a8c73
unknown <developer.mariano@gmail.com>
2023-02-13 15:02:59 -0300

    insert script for results

---
+++ tabuada.js
@@ -0,0 +1,18 @@
+
+let multiplicar = document.getElementById('start')
+const div = document.getElementById("tabuada")
+multiplicar.onclick = function(){
+    var multiplicador = document.getElementById('multiplicador').value
+    multiplicador = parseInt(multiplicador)
+    if(multiplicador>0 && multiplicador<=10){
+    var text = ''
+    for(var i=1; i<=10; i++){
+        var resultado = i*multiplicador
+        text = text+i+' * '+multiplicador+" = "+resultado+"<br>"
+        //console.log(text)
+    }
+    div.innerHTML = text;
+}else{
+    div.innerHTML = "ERRO: Insira um valor entre 1 e 10!!!";
+}
+}
